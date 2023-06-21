3378c9c3f07da4b88ebd17318fb76c7d2fc88a42
STOGOVEGOR <89977985+STOGOVEGOR@users.noreply.github.com>
2023-06-21 18:56:37 +0000

    upd

--- main.py
+++ main.py
@@ -19,35 +19,47 @@ delay = 10
 BUSBASE = {
   'Bus1': {
     'Kamenari': [0, 'end'],
-    'Bijela': [5,44],
-    'Baosici': [15,34],
-    'Djenovici': [18,31],
-    'Kumbor': [23,26],
-    'Zelenika': [26,23],
-    'Meljine': [28,20],
-    'Savina': [29,15],
+    'KamenariBeach': [2, 46],
+    'BijelaHTLPark': [5,44],
+    'BijelaIdea': [7,43],
+    'BijelaVoli': [9,42],
+    'BijelaHDL': [10,41],
+    'BijelaZager': [12,39],
+    'BijelaDetDom': [14,37],
+    'BaosiciPravac': [15,36],
+    'BaosiciIdea': [16,34],
+    'BaosiciPapagaja': [17,32],
+    'Djenovici': [18,30],
+    'Kumbor': [20,26],
+    'Zelenika': [22,22],
+    'Meljine': [25,19],
+    'Savina': [28,16],
+    'Citadel': [29,14],
     'Centar': [30,12],
     'Semafor': [32,10],
-    'IgaloPochta': [35,7],
+    'IgaloVojvodina': [34,8],
+    'IgaloPochta': [36,6],
     'IgaloInstitut': [38,3],
     'HDLVOLI': ['end',0]
   },
   'Bus2':{
-    'Meljine': [20,'end'],
-    'Savina': [22,15],
+    'Meljine': [19,'end'],
+    'Savina': [21,15],
+    'Citadel': [29,15],
     'Centar': [23,12],
     'Semafor': [25,10],
+    'IgaloVojvodina': [34,7],
     'IgaloPochta': [28,7],
     'IgaloInstitut': [31,3],
     'HDLVOLI': ['end',0]
   }
 }
 
-BUS1_IGALO = [530, 600, 630, 700, 730, 800, 830, 900, 930, 1000, 1030, 1100, 1130, 1200, 1230, 1300, 1330, 1400, 1430, 1500, 1530, 1600, 1630, 1700, 1730, 1800, 1830, 1900, 1930, 2000, 2030, 2100, 2130, 2200, 2300, 2400, 530, 600, 630, 700, 730]
-BUS1_IGALO_SUN = [600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2100, 2200, 2300, 2400, 530, 600, 630, 700, 730]
-BUS1_KAMEN = [520, 550, 620, 650, 720, 750, 820, 850, 920, 950, 1020, 1050, 1120, 1150, 1220, 1250, 1320, 1350, 1420, 1450, 1520, 1550, 1620, 1650, 1720, 1750, 1820, 1850, 1920, 1950, 2020, 2050, 2120, 2150, 2220, 2250, 2350, 2450, 520, 550, 620, 650, 720]
-BUS1_KAMEN_SUN = [550, 650, 750, 850, 950, 1050, 1150, 1250, 1350, 1450, 1550, 1650, 1750, 1850, 1950, 2050, 2150, 2250, 2350, 2450, 550, 650, 750, 850, 950]
-BUS2_CIRCLE = [605, 645, 725, 805, 845, 925, 1005, 1045, 1125, 1205, 1245, 1325, 1405, 1445, 1525, 1605, 1645, 1725, 1805, 1845, 1925, 2005, 2045, 605, 645, 725, 805, 845]
+BUS1_IGALO = (530, 600, 630, 700, 730, 800, 830, 900, 930, 1000, 1030, 1100, 1130, 1200, 1230, 1300, 1330, 1400, 1430, 1500, 1530, 1600, 1630, 1700, 1730, 1800, 1830, 1900, 1930, 2000, 2030, 2100, 2130, 2200, 2300, 2400, 530, 600, 630, 700, 730)
+BUS1_IGALO_SUN = (600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2100, 2200, 2300, 2400, 530, 600, 630, 700, 730)
+BUS1_KAMEN = (520, 550, 620, 650, 720, 750, 820, 850, 920, 950, 1020, 1050, 1120, 1150, 1220, 1250, 1320, 1350, 1420, 1450, 1520, 1550, 1620, 1650, 1720, 1750, 1820, 1850, 1920, 1950, 2020, 2050, 2120, 2150, 2220, 2250, 2350, 2450, 520, 550, 620, 650, 720)
+BUS1_KAMEN_SUN = (550, 650, 750, 850, 950, 1050, 1150, 1250, 1350, 1450, 1550, 1650, 1750, 1850, 1950, 2050, 2150, 2250, 2350, 2450, 550, 650, 750, 850, 950)
+BUS2_CIRCLE = (605, 645, 725, 805, 845, 925, 1005, 1045, 1125, 1205, 1245, 1325, 1405, 1445, 1525, 1605, 1645, 1725, 1805, 1845, 1925, 2005, 2045, 605, 645, 725, 805, 845)
 
 # ======== CALC SECTION ==========
 def get_current_time() -> datetime:
@@ -152,9 +164,9 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['stops'])
 def lets_start(message):
-  keyboard = telebot.types.InlineKeyboardMarkup(row_width=10)
+  keyboard = telebot.types.InlineKeyboardMarkup()
   keyboard.row(
-    telebot.types.InlineKeyboardButton('Kamenari', callback_data='Kamenari'))
+    telebot.types.InlineKeyboardButton('KAMENARI Ferry ⛴️', callback_data='Kamenari'))
   keyboard.row(
     telebot.types.InlineKeyboardButton('Bijela', callback_data='Bijela'),
     telebot.types.InlineKeyboardButton('Baosici', callback_data='Baosici'),
@@ -168,21 +180,49 @@ def lets_start(message):
     telebot.types.InlineKeyboardButton('Centar', callback_data='Centar'),
     telebot.types.InlineKeyboardButton('Semafor', callback_data='Semafor'))
   keyboard.row(
+    telebot.types.InlineKeyboardButton(text=f'Igalo\nVojvodn',
+                                       callback_data='IgaloVojvodina'),
     telebot.types.InlineKeyboardButton('Igalo Park',
                                        callback_data='IgaloPochta'),
     telebot.types.InlineKeyboardButton('Igalo Institut',
                                        callback_data='IgaloInstitut'))
   keyboard.row(
-    telebot.types.InlineKeyboardButton('HDL-VOLI', callback_data='HDLVOLI'))
-  print('who is using bot rigth now: ' + str(message.from_user.username))
+    telebot.types.InlineKeyboardButton('IGALO, HDL-VOLI 🛒', callback_data='HDLVOLI'))
+  print('query from: ' + str(message.from_user.username))
   bot.send_message(message.chat.id,
                    'Где вы сейчас?',
                    reply_markup=keyboard)
 
+def Bijela(message):
+  keyboard = telebot.types.InlineKeyboardMarkup(row_width=2)
+  btn1 = telebot.types.InlineKeyboardButton('Bijela Hotel Park', callback_data='BijelaHTLPark')
+  btn2 = telebot.types.InlineKeyboardButton('Bijela Idea', callback_data='BijelaIdea')
+  btn3 = telebot.types.InlineKeyboardButton('Bijela Voli', callback_data='BijelaVoli')
+  btn4 = telebot.types.InlineKeyboardButton('Bijela HDL', callback_data='BijelaHDL')
+  btn5= telebot.types.InlineKeyboardButton('Bijela Zager', callback_data='BijelaZager')
+  btn6= telebot.types.InlineKeyboardButton('Bijela Dječiji Dom', callback_data='BijelaDetDom')
+  keyboard.add(btn1, btn2, btn3, btn4, btn5, btn6)
+  bot.send_message(message.chat.id,
+                   'Уточните остановку',
+                   reply_markup=keyboard)
+
+
+def Baosici(message):
+  keyboard = telebot.types.InlineKeyboardMarkup(row_width=1)
+  btn1 = telebot.types.InlineKeyboardButton('Baosici Pravac', callback_data='BaosiciPravac')
+  btn2 = telebot.types.InlineKeyboardButton('Baosici Idea, Centar', callback_data='BaosiciIdea')
+  btn3 = telebot.types.InlineKeyboardButton('Baosici Papagaja', callback_data='BaosiciPapagaja')
+  keyboard.add(btn1, btn2, btn3)
+  bot.send_message(message.chat.id,
+                   'Уточните остановку',
+                   reply_markup=keyboard)
+
+
+@bot.message_handler(commands=['menu'])
 def mainmenu(message):
   keyboard = telebot.types.InlineKeyboardMarkup()
   keyboard.row(
-    telebot.types.InlineKeyboardButton('Выбрать местоположение', callback_data='newcall'))
+    telebot.types.InlineKeyboardButton('Выберите местоположение:', callback_data='new_call'))
   bot.send_message(message.chat.id,
                    'Новый запрос?',
                    reply_markup=keyboard)
@@ -201,42 +241,55 @@ def mainmenu(message):
 # ========== reply to buttons ============
 @bot.callback_query_handler(func=lambda call: True)
 def dialogue(call):
-  if call.data == 'newcall':
+  if call.data == 'new_call':
     lets_start(call.message)
-  msg_tmplt = 0
-  user = call.from_user.username
-  cur_time = get_current_time()
-  cur_hour = int(cur_time.strftime("%H")+'00')
-  if cur_hour == 0:
-    cur_hour = 2400
-  cur_minute = cur_time.strftime("%M")
-  day_of_week = cur_time.weekday()
-  print('time now is', cur_time)
-  bus1toigalo = when_next(call.data, 'Bus1', 'Igalo')
-  bus1toigalo_msg = f'🚌1️⃣:  {",  ".join(bus1toigalo)}\n'
-  bus1toferry = when_next(call.data, 'Bus1', 'Ferry')
-  bus1toferry_msg = f'🚌1️⃣:  {",  ".join(bus1toferry)}\n'
-
-  bus2toigalo = when_next(call.data, 'Bus2', 'Igalo')
-  bus2toferry = when_next(call.data, 'Bus2', 'Ferry')
+  if call.data == 'Bijela':
+    Bijela(call.message)
+  if call.data == 'Baosici':
+    Baosici(call.message)
+  else:
+    msg_tmplt = 0
+    user = call.from_user.username
+    cur_time = get_current_time()
+    cur_hour = int(cur_time.strftime("%H")+'00')
+    if cur_hour == 0:
+      cur_hour = 2400
+    cur_minute = cur_time.strftime("%M")
+    day_of_week = cur_time.weekday()
+    print('time now is', cur_time)
+    
+    bus1toigalo = when_next(call.data, 'Bus1', 'Igalo')
+    if bus1toigalo:
+      bus1toigalo_msg = f'🚌1️⃣:  {",  ".join(bus1toigalo)}\n'
+    else:
+      bus1toigalo_msg = 'no data'
+    bus1toferry = when_next(call.data, 'Bus1', 'Ferry')
+    if bus1toferry:
+      bus1toferry_msg = f'🚌1️⃣:  {",  ".join(bus1toferry)}\n'
+    else:
+      bus1toferry_msg = 'no data'
   
-  if day_of_week != 6 and bus2toigalo:
-    bus2toigalo_msg = f'🚌2️⃣:  {",  ".join(bus2toigalo)}\n'
-    bus2toferry_msg = f'🚌2️⃣:  {",  ".join(bus2toferry)}\n'
-    msg_tmplt = f'В сторону Каменари (паром ⛴️):\n' \
-    f'{bus1toferry_msg}' \
-    f'{bus2toferry_msg}\n'\
-    f'В сторону Игало (ХДЛ 🛒):\n' \
-    f'{bus1toigalo_msg}' \
-    f'{bus2toigalo_msg}\n'
-  if day_of_week == 6 or not bus2toigalo:
-    msg_tmplt = f'В сторону Каменари (паром ⛴️):\n' \
-    f'{bus1toferry_msg}\n' \
-    f'В сторону Игало (ХДЛ 🛒):\n' \
-    f'{bus1toigalo_msg}\n'
-
-  bot.send_message(call.message.chat.id, text=msg_tmplt)
-  mainmenu(call.message)
+    bus2toigalo = when_next(call.data, 'Bus2', 'Igalo')
+    bus2toferry = when_next(call.data, 'Bus2', 'Ferry')
+    
+    if day_of_week != 6 and bus2toigalo and bus1toigalo:
+      
+      bus2toigalo_msg = f'🚌2️⃣:  {",  ".join(bus2toigalo)}\n'
+      bus2toferry_msg = f'🚌2️⃣:  {",  ".join(bus2toferry)}\n'
+      msg_tmplt = f'В сторону Каменари (паром ⛴️):\n' \
+      f'{bus1toferry_msg}' \
+      f'{bus2toferry_msg}\n'\
+      f'В сторону Игало (ХДЛ 🛒):\n' \
+      f'{bus1toigalo_msg}' \
+      f'{bus2toigalo_msg}\n'
+    if day_of_week == 6 or not bus2toigalo:
+      msg_tmplt = f'В сторону Каменари (паром ⛴️):\n' \
+      f'{bus1toferry_msg}\n' \
+      f'В сторону Игало (ХДЛ 🛒):\n' \
+      f'{bus1toigalo_msg}\n'
+  
+    bot.send_message(call.message.chat.id, text=msg_tmplt)
+    mainmenu(call.message)
   
 
 
